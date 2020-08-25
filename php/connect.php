2601c8a37b000789e3347df820f74edae3fe957f
nakrute <70236119+nakrute@users.noreply.github.com>
2020-08-25 18:10:26 -0400

    Update connect.php

--- php/connect.php
+++ php/connect.php
@@ -6,12 +6,8 @@ $username = "ljhinkoxaubwmq";
 $password = "b74f2b65cdd29955d711d7e813b4bae5f28db37c953ffa13bb363ad2fe6e360a";
 $port = "5432"
 
-$db = parse_url(getenv("ec2-54-91-178-234.compute-1.amazonaws.com"));
-$db["path"] = ltrim($db["path"], "/");
-echo $db
-
 // Create connection
-$conn = new pg_connect("host=$hostname, dbname=$database, user=$username, password=$password");
+$conn = new pg_connect("host=$hostname dbname=$database user=$username password=$password port=$port");
 // Check connection
 echo "TEST";
 if ($conn->connect_error) {
