15178bb7b270171019d1bbcf3c6679f3aed4008b
nakrute <70236119+nakrute@users.noreply.github.com>
2020-08-25 16:57:17 -0400

    Update connect.php

--- php/connect.php
+++ php/connect.php
@@ -7,7 +7,7 @@ $password = "b74f2b65cdd29955d711d7e813b4bae5f28db37c953ffa13bb363ad2fe6e360a";
 $port = "5432"
 
 // Create connection
-$conn = new pg_connect(host=$hostname, dbname=$database, user=$username, password=$password);
+$conn = new pg_connect("host=$hostname, dbname=$database, user=$username, password=$password");
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
