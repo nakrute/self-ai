c7dfca253dee093464c270768948aaeb28965d66
nakrute <70236119+nakrute@users.noreply.github.com>
2020-08-25 21:59:09 -0400

    Update connect.php

--- php/connect.php
+++ php/connect.php
@@ -1,3 +1,14 @@
 <?php
-echo "TESTING";
+$db = parse_url(getenv("DATABASE_URL"));
+
+$pdo = new PDO("pgsql:" . sprintf(
+    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+    $db["host"],
+    $db["port"],
+    $db["user"],
+    $db["pass"],
+    ltrim($db["path"], "/")
+));
+
+die("CONNECTION WORKED")
 ?>
