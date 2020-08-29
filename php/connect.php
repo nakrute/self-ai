86d26b5d712e0ef9b155c2607227e1297f1ac5a5
nakrute <70236119+nakrute@users.noreply.github.com>
2020-08-28 22:01:13 -0400

    Update connect.php

--- php/connect.php
+++ php/connect.php
@@ -1,15 +1,14 @@
 <?php
-error_reporting(E_ALL);
-$servername = "ec2-54-91-178-234.compute-1.amazonaws.com";
-$username = "ljhinkoxaubwmq";
-$password = "b74f2b65cdd29955d711d7e813b4bae5f28db37c953ffa13bb363ad2fe6e360a";
-$dbname = "d5oiuu5n5pjd3a";
-$port = "5432";
+//error_reporting(E_ALL);
+//$servername = "ec2-54-91-178-234.compute-1.amazonaws.com";
+//$username = "ljhinkoxaubwmq";
+//$password = "b74f2b65cdd29955d711d7e813b4bae5f28db37c953ffa13bb363ad2fe6e360a";
+//$dbname = "d5oiuu5n5pjd3a";
+//$port = "5432";
 
 // Create connection
 $conn_string = "host=ec2-54-91-178-234.compute-1.amazonaws.com port=5432 dbname=d5oiuu5n5pjd3a user=ljhinkoxaubwmq password=b74f2b65cdd29955d711d7e813b4bae5f28db37c953ffa13bb363ad2fe6e360a";
-$conn = pg_connect($conn_string);
-	or die('Could not connect: ' . pg_last_error());
+$conn = pg_connect($conn_string) or die('Could not connect: ' . pg_last_error());
 
 //set blank values
 $routine_name = NULL;
