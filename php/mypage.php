6bd3b041b36f4ee81614c3b56ae036e24ea6e38c
nakrute <70236119+nakrute@users.noreply.github.com>
2020-08-28 22:49:31 -0400

    Update mypage.php

--- php/mypage.php
+++ php/mypage.php
@@ -6,6 +6,14 @@
 //$dbname = "d5oiuu5n5pjd3a";
 //$port = "5432";
 
+// Create connection
+$conn_string = "host=ec2-54-91-178-234.compute-1.amazonaws.com port=5432 dbname=d5oiuu5n5pjd3a user=ljhinkoxaubwmq password=b74f2b65cdd29955d711d7e813b4bae5f28db37c953ffa13bb363ad2fe6e360a";
+$conn = pg_connect($conn_string);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "SUCCESFULLY CONNECTED";
+
 //retrive the values from the form
 $searchcategory = mysqli_real_escape_string($conn, $_REQUEST['searchcategory']);
 //echo $searchcategory;
