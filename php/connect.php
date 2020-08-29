af29ceafe155aa45466588fcb43829f39f2f608a
nakrute <70236119+nakrute@users.noreply.github.com>
2020-08-28 21:58:42 -0400

    Update connect.php

--- php/connect.php
+++ php/connect.php
@@ -7,7 +7,8 @@ $dbname = "d5oiuu5n5pjd3a";
 $port = "5432";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn_string = "host=ec2-54-91-178-234.compute-1.amazonaws.com port=5432 dbname=d5oiuu5n5pjd3a user=ljhinkoxaubwmq password=b74f2b65cdd29955d711d7e813b4bae5f28db37c953ffa13bb363ad2fe6e360a";
+$conn = pg_connect($conn_string);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
