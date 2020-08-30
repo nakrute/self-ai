ec49e688c87bec7c3efc483bbebcfa6359472e3f
nakrute <70236119+nakrute@users.noreply.github.com>
2020-08-30 16:41:29 -0400

    Update connect.php

--- php/connect.php
+++ php/connect.php
@@ -12,7 +12,6 @@ $conn = pg_connect($conn_string);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "SUCCESFULLY CONNECTED";
 
 //set blank values
 $routine_name = NULL;
