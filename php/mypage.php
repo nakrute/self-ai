e2543d93db11e49c4299db63de709de2850ce9c0
nakrute <70236119+nakrute@users.noreply.github.com>
2020-08-30 18:58:57 -0400

    Update mypage.php

--- php/mypage.php
+++ php/mypage.php
@@ -1,5 +1,5 @@
 <?php
-error_reporting(0);
+//error_reporting(0);
 
 // Create connection
 $conn_string = "host=ec2-54-91-178-234.compute-1.amazonaws.com port=5432 dbname=d5oiuu5n5pjd3a user=ljhinkoxaubwmq password=b74f2b65cdd29955d711d7e813b4bae5f28db37c953ffa13bb363ad2fe6e360a";
