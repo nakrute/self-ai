b5895bb603dead11ceb97ef275f2947129498da9
nakrute <70236119+nakrute@users.noreply.github.com>
2020-08-30 21:14:34 -0400

    Update mypage.php

--- php/mypage.php
+++ php/mypage.php
@@ -23,7 +23,7 @@ echo $sql;
 $result = pg_query($conn, $sql);
 
 if(pg_num_rows($result) > 0) {
-	while ($row = pg_fetch_rows($result)) {
+	while ($row = pg_fetch_row($result)) {
 		echo $row;
 		if ($row['WTH'] == "Voice") 
 		{
